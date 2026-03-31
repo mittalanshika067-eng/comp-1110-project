cc3d4f3bd8c8f7d1f23decfe08650e79e7da31fe
mittalanshika067-eng <261872110+mittalanshika067-eng@users.noreply.github.com>
2026-03-31 12:28:43 +0800

    Add check_category_limits function for budget alerts
    Implement a function to check if spending exceeds budget limits per category and generate alerts.

--- alerts.py
+++ alerts.py
@@ -1,1 +1,31 @@
+#check is limit for any category is exceeded 
+#budget_rules: has list of rules with category and threshold in dictionary format
+def check_category_limits(transactions, budget_rules):
+   total_spending = {}                           #add total spending per category
+   for transaction in transactions:
+    category = transaction['category']
+    amount = transaction['amount']
 
+    if category in total_spending:                                  #once a category is in total_spending, amt will keep adding up
+       total_spending[category] = total_spending[category] + amount
+
+    else:
+      total_spending[category] = amount
+
+#total_spending will look like = { category : total_amount } 
+
+#check budget rules per category 
+    alerts = [] #list of alert messages 
+    
+    for rule in budget_rules:
+       category = rule['category']
+       limit = rule['threshold']
+
+  #get how much was spent and see if limit exceeded
+  #0 if category doesnt exist
+       spending = total_spending.get(category, 0)
+       if spending > limit:
+          alert = f"WARNING: You spent ${spending} on {category}. The budget you set was ${limit}"
+          alerts.append(alert)
+
+   return alerts
