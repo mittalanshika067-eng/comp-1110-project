3030f2ec7ecb1051e057c8fd1bc6519a288d96dd
Ukazu Miracle Oluebube <152558180+MiracleUkazu@users.noreply.github.com>
2026-04-21 11:50:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -221,7 +221,8 @@ def main():
     
     # Ask for large transaction limit if not set
     large_transaction_limit(budget_rules)
-    
+    print("Here are the possible Categories for transactions and budget rules. If your category does not fall under these you can select other and add a description manually. 
+    Categories: ")
     # Main menu loop
     while True:
         print("\n" + "-" * 40)
